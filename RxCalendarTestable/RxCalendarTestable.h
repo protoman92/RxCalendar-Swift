2ea7af27407afeb0168317db47ebf3adaa878141
Hai Pham <swiften.svc@gmail.com>
2018-05-02 16:55:56 +0800

    feat(testable): added RxCalendarTestable target

---
+++ RxCalendarTestable/RxCalendarTestable.h
@@ -0,0 +1,19 @@
+//
+//  RxCalendarTestable.h
+//  RxCalendarTestable
+//
+//  Created by Hai Pham on 5/2/18.
+//  Copyright © 2018 swiften. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxCalendarTestable.
+FOUNDATION_EXPORT double RxCalendarTestableVersionNumber;
+
+//! Project version string for RxCalendarTestable.
+FOUNDATION_EXPORT const unsigned char RxCalendarTestableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCalendarTestable/PublicHeader.h>
+
+
